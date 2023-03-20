783499ac9e10290eaee8f8c5b4f69711017d4335
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-03-20 19:08:54 +0000

    integrated the firebaseUI email login screen through the loginViewController and set the loginView controller as the delegate for it

--- photoapp/ViewControllers/LoginViewController.swift
+++ photoapp/ViewControllers/LoginViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuthUI
+import FirebaseEmailAuthUI
 
 class LoginViewController: UIViewController {
 
@@ -17,7 +19,55 @@ class LoginViewController: UIViewController {
  
     
     @IBAction func loginTapped(_ sender: Any) {
+        
+        // Create a firebase AuthUI obj
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        // Check that it isnt nil
+        if let authUI = authUI {
+            
+            // Set self as delegate for the authUI
+            authUI.delegate = self
+            
+            // Set sign in providers
+            authUI.providers = [FUIEmailAuth()]
+            
+            // Get the prebuilt UI view controller
+            let authViewController = authUI.authViewController()
+            
+            // Present it
+            present(authViewController, animated: true)
+            
+        }
+        
+        
+    }
+
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        
+        // Check if there was an error
+        if error != nil {
+            // There was an error
+            return
+        }
+        
+        let user = authDataResult?.user
+        
+        if let user = user {
+            
+            // Got a user
+            // Check on the database side if the user has a profile
+            
+            // If not, go to create profile vc
+            
+            // If so, go to tab bar controller
+        }
+        
     }
     
-
 }
