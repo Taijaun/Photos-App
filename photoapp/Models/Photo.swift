fcdf74c55cc5682ae9f229737501e206e8201283
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-03-23 18:56:39 +0000

    created a structure to store the photos information

---
+++ photoapp/Models/Photo.swift
@@ -0,0 +1,18 @@
+//
+//  Photo.swift
+//  photoapp
+//
+//  Created by Taijaun Pitt on 23/03/2023.
+//
+
+import Foundation
+
+struct Photo {
+    
+    var photoId: String?
+    var byId: String?
+    var byUsername: String?
+    var date: String?
+    var url: String?
+    
+}
